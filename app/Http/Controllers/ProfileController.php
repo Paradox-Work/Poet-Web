8dfb5b37e191014c2e61dba9df89495715097277
Paradox-Work <ipa23.a.vasiljevs@vtdt.edu.lv>
2026-09-18 10:21:24 +0300

    Modified to delete previous cover on update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 use App\Models\User;
@@ -84,9 +85,11 @@ class ProfileController extends Controller
         /** @var \Illuminate\Http\UploadedFile $cover */
         $cover = $data['cover'] ?? null;
 
-        if ($cover) {
-            $path = $cover->store('avatars/'.$user->id, 'public');
-            $user->update(['cover_path' => $path]);
+        if ($user->cover_path) {
+                Storage::disk('public')->delete($user->cover_path);
+            }
+        $path = $cover->store('user-'.$user->id, 'public');
+        $user->update(['cover_path' => $path]);
         }
 
         session('success', 'Cover image has been updated');
